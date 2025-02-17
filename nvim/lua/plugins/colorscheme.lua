9d2d0b39461789537a48cccf6d8e1d0335bafb93
Benjamin Kuzava <bkuzava@athenahealth.com>
2025-02-17 14:50:37 -0500

    initial nvim config

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- { "arcticicestudio/nord-vim" },
+  --
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "nord",
+  --   },
+  -- },
+
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+}
